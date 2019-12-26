e82e1a09a3b52b7163bfeb0d3d2229859671a46d
Xu <charlesxu86@163.com>
2019-12-26 15:00:37 +0800

    Initial commit

---
+++ TF_2_api.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+'''
+@Author  :   Xu
+ 
+@Software:   PyCharm
+ 
+@File    :   TF_2_api.py
+ 
+@Time    :   2019-12-26 14:56
+ 
+@Desc    :
+ 
+'''
+
+import tensorflow as tf
+from tensorflow import keras
